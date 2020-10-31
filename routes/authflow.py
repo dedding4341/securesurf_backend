289c96266b064d9b734f617e2657efae99867776
Taka Masagatani <tkrsk@live.com>
2020-10-30 23:38:03 -0700

    Added sign in and sign up url extensions, fixed

--- routes/authflow.py
+++ routes/authflow.py
@@ -1,7 +1,5 @@
 import pyrebase
-from flask import Flask, request, jsonify
 from .settings import FIREBASE_CONFIG
-app = Flask(__name__)
 
 firebase = pyrebase.initialize_app(FIREBASE_CONFIG)
 auth = firebase.auth()
@@ -10,22 +8,23 @@ def sign_in_auth(user_email, password):
     try:
         auth.sign_in_with_email_and_password(user_email, password)
         response = {}
-        response = 'Sign In Successful'
-        return jsonify(response)
+        response['MESSAGE'] = 'Sign In Successful'
+        return response
     except:
         response = {}
         response['ERROR'] = 'Invalid email/password combination'
-        return jsonify(response)
+        return response
 
 def sign_up_auth(user_email,password):
+    
     try:
         auth.create_user_with_email_and_password(user_email, password)
         response = {}
-        response = 'Sign Up Successful'
-        return jsonify(response)
+        response['MESSAGE'] = 'Sign Up Successful'
+        return response
     except:
         response = {}
         response['ERROR'] = 'Email already exists'
-        return jsonify(response)
+        return response
 
 
