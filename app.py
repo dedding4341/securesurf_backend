2e01bfc78dcf4a7d0ccbf731f3d2da325c90ee15
dedding4341 <dedding4341@gmail.com>
2020-10-31 01:38:19 -0700

    ack_branch ready!

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, json
 from flask_cors import CORS
 from routes import breaches
 from routes import safebrowsing
+from routes import datastore
 app = Flask(__name__)
 
 CORS(app)
@@ -26,10 +27,36 @@ def analyze_url():
     response = safebrowsing.safety_analysis(user_email=user_email, visited_url=url)
     return jsonify(response)
 
+@app.route('/ack_breach', methods=['POST'])
+def acknowledge_breach():
+    request_content = request.get_json(silent=False)
+
+    user_email = request_content.get('user_email', None)
+    breach_name = request_content.get('breach_name', None)
+
+    if not user_email:
+        response = {}
+        response['ERROR'] = 'No email found'
+        return jsonify(response)
+
+    if not breach_name:
+        response = {}
+        response['ERROR'] = 'No breach name found'
+        return jsonify(response)
+
+    result = datastore.ack_breach(user_email=user_email, breach_name=breach_name)
+    if result:
+        response = {}
+        response['MESSAGE'] = 'Acknowledgement received'
+        return jsonify(response)
+    response = {}
+    response['ERROR'] = 'Acknowledgement received, but error occurred'
+    return jsonify(response)
+
+
 @app.route('/breaches', methods=['GET'])
 def find_user_breaches():
     request_content = request.get_json(silent=False)
-    print(request_content)
 
     user_email = request_content.get('user_email', None)
     
--- routes/datastore.py
+++ routes/datastore.py
@@ -44,4 +44,50 @@ def load_compromised_sites(user_email, detailed_breach_info):
         updated_names = unacknowledged_names + list(set(breach_names) - set(unacknowledged_names))
         db.child("users").child(user_email).child("unacknowledged").update({"breach_names": updated_names})
 
+def sanitize_return(user_email, detailed_breach_info):
+    acknowledged_data = None
+    unacknowledged_data = None
+    
+    # strip forbidden characters for DB interactions
+    user_email = user_email.replace('@', '')
+    user_email = user_email.replace('.', '')
+    
+    try:
+        unacknowledged_data = db.child("users").child(user_email).child("unacknowledged").get()
+        unacknowledged_names = unacknowledged_data.val().get('breach_names', None)
+    except:
+        unacknowledged_names = None
 
+    return unacknowledged_names
+    
+def ack_breach(user_email, breach_name):
+    acknowledged_data = None
+    unacknowledged_data = None
+    
+    # strip forbidden characters for DB interactions
+    user_email = user_email.replace('@', '')
+    user_email = user_email.replace('.', '')
+    
+    try:
+        unacknowledged_data = db.child("users").child(user_email).child("unacknowledged").get()
+        unacknowledged_names = unacknowledged_data.val().get('breach_names', None)
+    except:
+        unacknowledged_names = None
+    
+    try:
+        acknowledged_data = db.child("users").child(user_email).child("acknowledged").get()
+        acknowledged_names = acknowledged_data.val().get('breach_names', None)
+    except:
+        acknowledged_names = None
+
+    try:
+        unacknowledged_names.remove(breach_name)
+        acknowledged_names.append(breach_name)
+
+        db.child("users").child(user_email).child("unacknowledged").update({"breach_names": unacknowledged_names})
+        db.child("users").child(user_email).child("acknowledged").update({"breach_names": acknowledged_names})
+        return True
+    except:
+        return False
+    
+    
