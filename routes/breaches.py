c47367cde902a8db4bc305d4bbb304ed6ddbec36
dedding4341 <dedding4341@gmail.com>
2020-10-31 00:48:28 -0700

    init datastore

--- routes/breaches.py
+++ routes/breaches.py
@@ -1,12 +1,13 @@
 from .settings import HIBP_KEY
 import pypwned as breach_service
+from .datastore import load_compromised_sites
 
 breach_service = breach_service.pwned(HIBP_KEY)
 
 def get_all_breaches_for_user(user_email):
-    return build_detailed_breach_information(breach_service.getAllBreachesForAccount(email=user_email))
+    return build_detailed_breach_information(user_email, breach_service.getAllBreachesForAccount(email=user_email))
 
-def build_detailed_breach_information(breach_list):
+def build_detailed_breach_information(user_email, breach_list):
 
     # This needs to be run by acknowledged vs unacknowledged list in firebase
 
@@ -14,4 +15,8 @@ def build_detailed_breach_information(breach_list):
     for breach in breach_list:
         detailed_breach = breach_service.getSingleBreachedSite(name=breach['Name'])
         detailed_breach_info.append(detailed_breach)
+
+    # Sanitize + update list against acknowledged and unacknowledged in firebase
+    print("running")
+    load_compromised_sites(user_email=user_email, detailed_breach_info=breach_list)
     return detailed_breach_info
---
+++ routes/datastore.py
@@ -0,0 +1,47 @@
+import pyrebase
+from .settings import FIREBASE_CONFIG
+
+firebase = pyrebase.initialize_app(FIREBASE_CONFIG)
+db = firebase.database()
+
+def load_compromised_sites(user_email, detailed_breach_info):
+    acknowledged_data = None
+    unacknowledged_data = None
+    
+    # strip forbidden characters for DB interactions
+    user_email = user_email.replace('@', '')
+    user_email = user_email.replace('.', '')
+
+    try:
+        acknowledged_data = db.child("users").child(user_email).child("acknowledged").get()
+        acknowledged_names = acknowledged_data.val().get('breach_names', None)
+    except:
+        acknowledged_names = None
+    
+    try:
+        unacknowledged_data = db.child("users").child(user_email).child("unacknowledged").get()
+        unacknowledged_names = unacknowledged_data.val().get('breach_names', None)
+    except:
+        unacknowledged_names = None
+
+    breach_names = []
+
+    for breach in detailed_breach_info:
+        if acknowledged_names and breach['Name'] in acknowledged_names:
+            continue
+        elif unacknowledged_names and breach['Name'] in unacknowledged_names:
+            continue
+        else:
+            breach_names.append(breach['Name'])
+    
+    if len(breach_names) == 0:
+        return
+
+    # init new datastore
+    if not unacknowledged_names:
+        db.child("users").child(user_email).child("unacknowledged").set({"breach_names": breach_names})
+    else:
+        updated_names = unacknowledged_names + list(set(breach_names) - set(unacknowledged_names))
+        db.child("users").child(user_email).child("unacknowledged").update({"breach_names": updated_names})
+
+
